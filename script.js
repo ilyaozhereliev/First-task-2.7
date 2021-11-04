0e672f09fc676d010a88f48e87b337831c1c0183
Iluka <ilyaozhereliev@gmail.com>
2021-11-04 16:24:10 +0300

    beautyfied code

--- script.js
+++ script.js
@@ -2,88 +2,116 @@
 let money, time;
 
 function start() {
-    money = +prompt('Ваш бюджет на месяц?');
-    time = +prompt('Введите дату в формате YYYY-MM-DD');
+	money = +prompt('Ваш бюджет на месяц?');
+	time = +prompt('Введите дату в формате YYYY-MM-DD');
 
-    while(isNaN(money) || money === '' || money === null) {
-        money = +prompt('Ваш бюджет на месяц?'); 
-    }
+	while (isNaN(money) || money === '' || money === null) {
+		money = +prompt('Ваш бюджет на месяц?');
+	}
 }
 start();
 
-
 const appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true,
-    chooseExpenses: function() {
-        for (let i = 0; i < 2; i++) {
-            let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-            let b = +prompt('Во сколько это обойдется?', '');
-        
-            if ( (typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null && a !== '' && b !== '' && a.length < 50) {
-                appData.expenses[a] = b;
-            } else {
-                i--;
-            }
-        }
-    },
-    detectDayBudget: function() {
-        appData.moneyPerDay = (appData.budget / 30).toFixed();
-        alert(`Ежедневный бюджет: ${appData.moneyPerDay} рублей`);
-    },
-    detectLevel: function() {
-        if (appData.moneyPerDay < 100) {
-            console.log(`Минимальный уровень достатка`);
-        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            console.log(`Средний уровень достатка`);
-        } else if (appData.moneyPerDay > 2000) {
-            console.log(`Высокий уровень достатка`);
-        } else {
-            console.log(`Произошла ошибка`);
-        }
-    },
-    checkSavings: function() {
-        if(appData.savings === true) {
-            let save = +prompt('Какова сумма накоплений?');
-            let percent = +prompt('Под какой процент?');
-    
-            appData.mounthIncome = save / 100 / 12 * percent;
-            alert(`Доход в месяц с вашего депозита: ${appData.mounthIncome}`);
-        }
-    },
-    chooseOptExpenses: function() {
-        for (let i = 0; i < 3; i++) {
-            let a = prompt('Статья необязательных расходов в этом месяце?');
-            let b = +prompt('Во сколько это обойдется?');
-    
-            if ( (typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null && a !== '' && b !== '' && a.length < 50) {
-                appData.optionalExpenses[a] = b;
-            } else {
-                i--;
-            }
-        }
-    },
-    chooseIncome: function() {
-            let items = prompt(`Что принесет дополнительный доход? (Перечислите через запятую.)`);
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: true,
+	chooseExpenses: function () {
+		for (let i = 0; i < 2; i++) {
+			let a = prompt(
+				'Введите обязательную статью расходов в этом месяце',
+				''
+			);
+			let b = +prompt('Во сколько это обойдется?', '');
 
-            while (typeof(items) !== 'string' || items === '' || typeof(items) === null) {
-                alert(`Вы не можете ввести числа и оставить строку пустой. Пожалуйста, ответьте еще раз.`);
-                items = prompt(`Что принесет дополнительный доход? (Перечислите через запятую.)`);
-            }
-                appData.income = items.split(', ');
-                appData.income.push(prompt(`Может, что-то еще?`));
-                appData.income.sort();
+			if (
+				typeof a === 'string' &&
+				typeof a !== null &&
+				typeof b !== null &&
+				a !== '' &&
+				b !== '' &&
+				a.length < 50
+			) {
+				appData.expenses[a] = b;
+			} else {
+				i--;
+			}
+		}
+	},
+	detectDayBudget: function () {
+		appData.moneyPerDay = (appData.budget / 30).toFixed();
+		alert(`Ежедневный бюджет: ${appData.moneyPerDay} рублей`);
+	},
+	detectLevel: function () {
+		if (appData.moneyPerDay < 100) {
+			console.log(`Минимальный уровень достатка`);
+		} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+			console.log(`Средний уровень достатка`);
+		} else if (appData.moneyPerDay > 2000) {
+			console.log(`Высокий уровень достатка`);
+		} else {
+			console.log(`Произошла ошибка`);
+		}
+	},
+	checkSavings: function () {
+		if (appData.savings === true) {
+			let save = +prompt('Какова сумма накоплений?');
+			let percent = +prompt('Под какой процент?');
 
-                appData.income.forEach(function(item, i) {
-                    alert(`Способы дополнительного заработка: ${i + 1}: ${item}`);
-                });
-    } 
+			appData.mounthIncome = (save / 100 / 12) * percent;
+			alert(`Доход в месяц с вашего депозита: ${appData.mounthIncome}`);
+		}
+	},
+	chooseOptExpenses: function () {
+		for (let i = 0; i < 3; i++) {
+			let a = prompt('Статья необязательных расходов в этом месяце?');
+			let b = +prompt('Во сколько это обойдется?');
+
+			if (
+				typeof a === 'string' &&
+				typeof a !== null &&
+				typeof b !== null &&
+				a !== '' &&
+				b !== '' &&
+				a.length < 50
+			) {
+				appData.optionalExpenses[a] = b;
+			} else {
+				i--;
+			}
+		}
+	},
+	chooseIncome: function () {
+		let items = prompt(
+			`Что принесет дополнительный доход? (Перечислите через запятую.)`
+		);
+
+		while (
+			typeof items !== 'string' ||
+			items === '' ||
+			typeof items === null
+		) {
+			alert(
+				`Вы не можете ввести числа и оставить строку пустой. Пожалуйста, ответьте еще раз.`
+			);
+			items = prompt(
+				`Что принесет дополнительный доход? (Перечислите через запятую.)`
+			);
+		}
+		appData.income = items.split(', ');
+		appData.income.push(prompt(`Может, что-то еще?`));
+		appData.income.sort();
+
+		appData.income.forEach(function (item, i) {
+			alert(`Способы дополнительного заработка: ${i + 1}: ${item}`);
+		});
+	},
 };
 
 for (let key in appData) {
-    console.log(`Наша программа включает в себя данные: ${key} - ${appData[key]}`);
+	console.log(
+		`Наша программа включает в себя данные: ${key} - ${appData[key]}`
+	);
 }
